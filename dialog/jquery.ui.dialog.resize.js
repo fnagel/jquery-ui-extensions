e6ee1474bf43b5c839b2b31d191e57e01a820337
Felix Nagel <info@felixnagel.com>
2013-07-03 21:09:57 +0200

    init extension and add test files

---
+++ dialog/jquery.ui.dialog.resize.js
@@ -0,0 +1,16 @@
+/*
+ * jQuery UI Dialog Resize Extension
+ *
+ * Copyright 2013, Felix Nagel (http://www.felixnagel.com)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * http://github.com/fnagel/jquery-ui-extensions
+ */
+(function( $ ) {
+
+$.widget( "ui.dialog", $.ui.dialog, {
+
+	
+});
+
+}( jQuery ));
