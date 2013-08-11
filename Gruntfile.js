d009f64ccd2fbae82fed8d12243af1d328257e94
Felix Nagel <info@felixnagel.com>
2013-08-12 00:54:49 +0200

    lint fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,31 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
-		pkg : grunt.file.readJSON('package.json'),
+		pkg : grunt.file.readJSON("package.json"),
 		concat : {
 			options : {
-				separator : ';',
-				banner : '/*! v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy HH:MM") %> */\n'
+				separator : ";",
+				banner : "/*! v<%= pkg.version %> - <%= grunt.template.today('dd-mm-yyyy HH:MM') %> */\n"
 			},
 			dist : {
-				src : ['dialog/**/*.js'],
-				dest : 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
+				src : ["dialog/**/*.js"],
+				dest : "dist/<%= pkg.name %>-<%= pkg.version %>.js"
 			}
 		},
 		uglify : {
 			options: {
-				report: 'min',
-				preserveComments : 'some'
-				
+				report: "min",
+				preserveComments : "some"
+
 			},
 			dist : {
 				files : {
-					'dist/<%= pkg.name %>-<%= pkg.version %>.min.js' : ['<%= concat.dist.dest %>']
+					"dist/<%= pkg.name %>-<%= pkg.version %>.min.js" : ["<%= concat.dist.dest %>"]
 				}
 			}
 		},
-		jshint : {	
-			files : ['gruntfile.js', 'dialog/**/*.js'],
+		jshint : {
+			files : ["gruntfile.js", "dialog/**/*.js"],
 			options : {
 				"boss": true,
 				"curly": true,
@@ -50,27 +50,27 @@ module.exports = function (grunt) {
 		compress: {
 			main: {
 				options: {
-					archive: '<%= pkg.name %>-<%= pkg.version %>.zip'
+					archive: "<%= pkg.name %>-<%= pkg.version %>.zip"
 				},
 				files: [
-					{ src: ['./**', '!./node_modules/**', '!./*.zip'], dest: '<%= pkg.name %>-<%= pkg.version %>/' }
+					{ src: ["./**", "!./node_modules/**", "!./*.zip"], dest: "<%= pkg.name %>-<%= pkg.version %>/" }
 				]
 			}
 		},
 		watch : {
-			files : ['<%= jshint.files %>'],
-			tasks : ['jshint']
+			files : ["<%= jshint.files %>"],
+			tasks : ["jshint"]
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-compress");
 
-	grunt.registerTask('test', ['jshint']);
+	grunt.registerTask("test", ["jshint"]);
 
-	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'compress']);
+	grunt.registerTask("default", ["jshint", "concat", "uglify", "compress"]);
 
 };
