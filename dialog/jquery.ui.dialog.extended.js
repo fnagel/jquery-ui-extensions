63e038f2626278fd317b18e701e4f4427bd46210
Felix Nagel <info@felixnagel.com>
2013-08-28 00:46:39 +0200

    lint fix

--- dialog/jquery.ui.dialog.extended.js
+++ dialog/jquery.ui.dialog.extended.js
@@ -275,7 +275,7 @@ $.widget( "ui.dialog", $.ui.dialog, {
 	},
 
 	_windowResizeEvents: {
-		resize: function( event ){
+		resize: function(){
 			if ( this.options.resizeOnWindowResize ) {
 				this._addTimeout( function() {
 					this._setOptions( this._oldSize );
