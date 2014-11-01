139590bd3556cf17eeb7d8e67fc89cfd875c27fe
Felix Nagel <info@felixnagel.com>
2014-11-01 13:56:57 +0100

    Add a basic implementation of a native menu extension

---
+++ selectmenu/selectmenu.native-menu.js
@@ -0,0 +1,67 @@
+/*
+ * jQuery UI Selectmenu Native Menu Extension
+ *
+ * Copyright 2012-2014, Felix Nagel (http://www.felixnagel.com)
+ * Released under the MIT license.
+ *
+ * http://github.com/fnagel/jquery-ui-extensions
+ */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([
+			"jquery",
+			"jqueryui/ui/core",
+			"jqueryui/ui/widget",
+			"jqueryui/ui/selectmenu"
+		], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery );
+	}
+}(function( $ ) {
+
+return $.widget( "ui.selectmenu", $.ui.selectmenu, {
+	// add options
+	options: {
+		nativeMenu: false
+
+		// Callbacks
+		// not available when using native menu mode
+	},
+
+	_drawButton: function() {
+		this._super();
+
+		this.element
+			.css({
+				position: "absolute",
+				top: 0,
+				opacity: 0.001
+			})
+			.show();
+
+		this.element.appendTo( this.button );
+
+
+		this._off( this.button );
+		this.button.off( "focusin" );
+	},
+
+	_position: function() {
+		if ( !this.options.nativeMenu ) {
+			this._super();
+			return;
+		}
+	},
+
+	_drawMenu: function() {
+		if ( !this.options.nativeMenu ) {
+			this._super();
+		}
+	}
+});
+
+}));
